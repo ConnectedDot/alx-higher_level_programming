18147c18a9d7a5ce41dd4502edc9fa6f4d09b35d
ConnectedDot <psalmomo@gmail.com>
2023-06-12 07:35:28 +0100

    Depencies for python 3 on data structures

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,17 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * for project
  */
 typedef struct listint_s
 {
@@ -23,7 +19,6 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
-void reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
