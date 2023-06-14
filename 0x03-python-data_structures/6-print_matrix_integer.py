08bde19da655125e449abc0cb1a24ac67dab2c5a
ConnectedDot <psalmomo@gmail.com>
2023-06-14 05:11:17 +0100

    ALigned

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,11 +6,7 @@ def print_matrix_integer(zqmatrix=[[]]):
     - zqmatrix: A list of elements of the matrix
     (optional - this might be an empty array if no matrix)
     """
-
     for row in zqmatrix:
-        for i in range(len(row)):
-            if i == len(row) - 1:
-                print("{:d}".format(row[i]), end="")
-        else:
-            print("{:d}".format(row[i]), end=" ")
-    print()
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,21 +1,7 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    """
-    A function to add multple tuple.
-
-    Arguments:
-    - tuple_a: a tuple containing two integers (default is an empty tuple)
-    - tuple_b: a tuple containing two integers (default is an empty tuple)
-
-    Return:
-    - a tuple containing two integers where the first element is the sum of the
-      first element in each tuple, and the second element is the sum of the
-      second element in each tuple.
-    """
-
-    a = tuple_a[0] if len(tuple_a) >= 1 else 0
-    b = tuple_a[1] if len(tuple_a) >= 2 else 0
-    c = tuple_b[0] if len(tuple_b) >= 1 else 0
-    d = tuple_b[1] if len(tuple_b) >= 2 else 0
-
-    return (a + c, b + d)
+    new_tuple = ()
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
+    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+    return new_tuple
