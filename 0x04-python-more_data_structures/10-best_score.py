64411daf58513c64b7a7a9097a6aff729d5ddafd
ConnectedDot <psalmomo@gmail.com>
2023-06-17 02:14:51 +0100

    10-best_score.py File

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,10 +6,10 @@ def best_score(a_dictionary):
     This function returns a key with the biggest integer value.
     """
     if a_dictionary:
-        my_list = list(a_dictionary.keys())
+        best_list = list(a_dictionary.keys())
         score = 0
         leader = ""
-        for i in my_list:
+        for i in best_list:
             if a_dictionary[i] > score:
                 score = a_dictionary[i]
                 leader = i
