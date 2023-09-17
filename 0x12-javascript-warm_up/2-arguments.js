602556554ba7bbd51eef4d2bc55f0ec0ed022e85
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2023-09-17 17:42:15 +0100

    Args length

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
-  console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
+
+const argsLength = process.argv.length;
+
+if (argsLength === 2) {
+  console.log("No argument");
+} else if (argsLength === 3) {
+  console.log("Argument found");
 } else {
-  console.log('Arguments found');
+  console.log("Arguments found");
 }
