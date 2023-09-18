6a0ee82c756f1bc6f2d2ca04b949a98f112a2768
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2023-09-18 16:30:45 +0100

    multiple argument

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
+
+const arg2 = process.argv[2];
+const x = parseInt(arg2);
+
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
 } else {
-  const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
-    console.log('C is fun');
-    i++;
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
   }
 }
