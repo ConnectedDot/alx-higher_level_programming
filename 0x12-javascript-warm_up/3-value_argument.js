df23b01da68ba83633d1131dd8aedc3d2cbd33a5
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2023-09-18 07:23:43 +0100

    value argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
-  console.log('No argument');
+
+const firstArg = process.argv[2];
+
+if (firstArg) {
+  console.log(firstArg);
 } else {
-  console.log(process.argv[2]);
+  console.log("No argument");
 }
