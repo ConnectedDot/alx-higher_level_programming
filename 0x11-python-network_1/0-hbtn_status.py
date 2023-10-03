9006f4c13a2eed3b5d79955ca308a050a2fca137
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2023-10-03 07:49:47 +0100

    Fetches a script using urllib package

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """A script that
 - fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
+- uses urllib package
 """
-
 
 if __name__ == '__main__':
     import urllib.request
