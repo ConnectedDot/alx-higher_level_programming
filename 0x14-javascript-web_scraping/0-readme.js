44b0e31dfecfed862bc1980ebfe6d3ee09aad618
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2024-02-05 10:56:01 +0100

    Checking checks

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Read from file
 
-const fs = require('fs');
+const fs = require("fs");
 
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
+fs.readFile(process.argv[2], "utf8", function (err, data) {
   if (err) {
     console.log(err);
   } else {
